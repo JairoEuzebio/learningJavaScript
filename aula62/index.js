b82df6a341e333ed2cfd5f6c78d3276afbc31204
JairoEuzebio <jairo9783@gmail.com>
2025-03-20 20:51:39 +0000

    Added from class 61 to 64

---
+++ aula62/index.js
@@ -0,0 +1,21 @@
+// Class 62 - Arrays (Revision)
+
+/*
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const novo = [...nomes];
+
+
+novo.pop();
+console.log(nomes);
+console.log(novo);
+*/ 
+
+/*
+const nome = 'Luiz Otávio Miranda';
+const nomes = nome.split(' '); // split transforma a string em um array
+console.log(nomes);
+*/
+
+const nomes = ['Luiz', 'Otávio', 'Miranda'];
+const nome = nomes.join(' '); // join transforma o array em uma string
+console.log(nome);
---
+++ aula63/aula63.js
@@ -0,0 +1,5 @@
+// Class 63, Arrays - Splice
+
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(indice, delete, elem1, elem2, elem3);
